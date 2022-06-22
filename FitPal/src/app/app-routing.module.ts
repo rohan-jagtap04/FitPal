5dc07f827a04cbaf6f6b070ad2a1b35afbc524b7
Rohan <rrjagtap@uwaterloo.ca>
2022-06-21 23:28:58 -0700

    Working through dependencies

--- FitPal/src/app/app-routing.module.ts
+++ FitPal/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homepage',
+    loadChildren: () => import('../../projects/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('../../projects/authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: '',
+    redirectTo: 'authentication',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
